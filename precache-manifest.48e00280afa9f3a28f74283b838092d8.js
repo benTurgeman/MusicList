304d0579744d9cf0702aa7005c4689c41111cbcc
unknown <ben123tu@gmail.com>
2018-11-11 22:12:29 +0200

    Updates

---
+++ precache-manifest.48e00280afa9f3a28f74283b838092d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "37ef43fce2645cf2c64e",
+    "url": "/MusicList/static/js/runtime~main.37ef43fc.js"
+  },
+  {
+    "revision": "981d2c6c705161b648f4",
+    "url": "/MusicList/static/js/main.981d2c6c.chunk.js"
+  },
+  {
+    "revision": "2c822f4364ad8c5bdccd",
+    "url": "/MusicList/static/js/1.2c822f43.chunk.js"
+  },
+  {
+    "revision": "981d2c6c705161b648f4",
+    "url": "/MusicList/static/css/main.acec47df.chunk.css"
+  },
+  {
+    "revision": "59f44a61ff37a5e66c785d132dbdfd4e",
+    "url": "/MusicList/index.html"
+  }
+];
